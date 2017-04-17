8b04ceb7cfdb0a49496633e9030e1d04da397ff4
Joshisadesigner <joshisadesigner@gmail.com>
2017-04-17 08:18:07 -0600

    Add resume objects and resume image

--- app/js/resumeBuilder.js
+++ app/js/resumeBuilder.js
@@ -1,3 +1,76 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+var bio = {
+    "name": "Josh",
+    "role": "Front-End Web Developer",
+    "contact": [{
+        "mobile": "4276-8065",
+        "email": "joshisadesigner@me.com",
+        "twitter": "@joshisadesigner",
+        "github": "joshisadesigner",
+        "location": "Guatemala"
+    }],
+    "bioPicture": "images/me.jpg",
+    "welcomeMessage": "Welcome, I\"m learning front-end development!",
+    "skills": [
+        "Photoshop",
+        "Illustrator",
+        "InDesign",
+        "Sketch",
+        "HTML",
+        "CSS",
+        "JavaScript"
+    ]
+}
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Secure IP",
+            "title": "Front-End Web Developer",
+            "location": "Guatemala City, Guatemala.",
+            "dates": "August 2016 to Date",
+            "description": "",
+        },
+        {
+            "employer": "Social Media Networks, Inc.",
+            "title": "UI/UX Designer",
+            "location": "Guatemala City, Guatemala.",
+            "dates": "June 2015 to August 2016",
+            "description": ,
+        }
+    ]
+}
+
+var education = {
+    "schools": [
+        {
+            "name": "Universidad de San Carlos de Guatemala",
+            "location": "Guatemala City, Guatemala.",
+            "degreeDate": "August 2014",
+            "url": "http://usac.edu.gt/",
+            "majors": [ "BA" ]
+        }
+    ],
+    "onlineCourses": [
+        {
+            "name": "Front-End Web Developer, Nanodegree",
+            "location": "Guatemala City, Guatemala.",
+            "degreeDate": "January 2017",
+            "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001",
+            "majors": []
+        }
+    ]
+}
+
+var projects: {
+    {
+        "title": ,
+        "date": ,
+        "description": ,
+        "images": []
+    }
+}
