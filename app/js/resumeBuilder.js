dd1d95e881b19808f03f12ce40440ec0c31ef0e0
Josh <joshisadesigner@gmail.com>
2017-04-28 17:30:33 -0600

    Initialize Pagespeed Insights quiz

--- app/js/resumeBuilder.js
+++ app/js/resumeBuilder.js
@@ -191,34 +191,97 @@ projects.display();
 
 $('#mapDiv').append(googleMap);
 
+var psiResults;
 
-
-var moonWalkers = [
-  "Neil Armstrong",
-  "Buzz Aldrin",
-  "Pete Conrad",
-  "Alan Bean",
-  "Alan Shepard",
-  "Edgar Mitchell",
-  "David Scott",
-  "James Irwin",
-  "John Young",
-  "Charles Duke",
-  "Eugene Cernan",
-  "Harrison Schmitt"
-];
-
-function alphabetizer(names) {
-    var newArray = [];
-    names.forEach( function( element ){
-        nameArray = element.split( ' ' );
-        first = nameArray.slice( 0, 1 );
-        last = nameArray.slice( 1 );
-        fullName = last + ', ' + first;
-        newArray.push( fullName );
-        return newArray.sort();
-    });
+psiResults = {
+    "kind": "pagespeedonline#result",
+    "id": "/speed/pagespeed",
+    "responseCode": 200,
+    "title": "PageSpeed Home",
+    "score": 90,
+    "pageStats": {
+        "numberResources": 22,
+        "numberHosts": 7,
+        "totalRequestBytes": "2761",
+        "numberStaticResources": 16,
+        "htmlResponseBytes": "91981",
+        "cssResponseBytes": "37728",
+        "imageResponseBytes": "13909",
+        "javascriptResponseBytes": "247214",
+        "otherResponseBytes": "8804",
+        "numberJsResources": 6,
+        "numberCssResources": 2
+    },
+    "formattedResults": {
+        "locale": "en_US",
+        "ruleResults": {
+            "AvoidBadRequests": {
+                "localizedRuleName": "Avoid bad requests",
+                "ruleImpact": 0.0
+            },
+            "MinifyJavaScript": {
+                "localizedRuleName": "Minify JavaScript",
+                "ruleImpact": 0.1417,
+                "urlBlocks": [{
+                    "header": {
+                        "format": "Minifying the following JavaScript resources could reduce their size by $1 ($2% reduction).",
+                        "args": [{
+                                "type": "BYTES",
+                                "value": "1.3KiB"
+                            },
+                            {
+                                "type": "INT_LITERAL",
+                                "value": "0"
+                            }
+                        ]
+                    },
+                    "urls": [{
+                            "result": {
+                                "format": "Minifying $1 could save $2 ($3% reduction).",
+                                "args": [{
+                                        "type": "URL",
+                                        "value": "http://code.google.com/js/codesite_tail.pack.04102009.js"
+                                    },
+                                    {
+                                        "type": "BYTES",
+                                        "value": "717B"
+                                    },
+                                    {
+                                        "type": "INT_LITERAL",
+                                        "value": "1"
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            "result": {
+                                "format": "Minifying $1 could save $2 ($3% reduction).",
+                                "args": [{
+                                        "type": "URL",
+                                        "value": "http://www.gmodules.com/ig/proxy?url\u003dhttp%3A%2F%2Fjqueryjs.googlecode.com%2Ffiles%2Fjquery-1.2.6.min.js"
+                                    },
+                                    {
+                                        "type": "BYTES",
+                                        "value": "258B"
+                                    },
+                                    {
+                                        "type": "INT_LITERAL",
+                                        "value": "0"
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }]
+            },
+            "SpriteImages": {
+                "localizedRuleName": "Combine images into CSS sprites",
+                "ruleImpact": 0.0
+            }
+        }
+    },
+    "version": {
+        "major": 1,
+        "minor": 11
+    }
 }
-
-// Try logging your results to test your code!
-console.log(alphabetizer(moonWalkers));
